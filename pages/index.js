9190694cfe826375cca1b472d437c895d2f4fff7
Fabiano Rodrigues <fabianorr87@hotmail.com>
2023-05-09 10:08:25 -0300

    feat: first commit

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+
+import HomeScreen from "../src/screens/HomeScreen";
+
+export default HomeScreen;
+
+// function Title({ children, as }) {
+//   const Tag = as;
+//   return (
+//     <>
+//       <Tag>
+//         {children}
+//       </Tag>
+//       <style jsx>
+//         {`
+//           ${Tag} {
+//             color: red;
+//             font-family: sans-serif;
+//           }
+//         `}
+//       </style>
+//     </>
+//   );
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h1">Alura Cases - Home</Title>
+//       <Link href="/faq">Ir para FAQ</Link>
+//       <Footer />
+//     </div>
+//   );
+// }
